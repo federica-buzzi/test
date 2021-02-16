8d607d341cd1244e8473ec1db7589103edd40757
Daniel Sum <daniel@lacerisenumerique.be>
2021-02-16 13:08:06 +0100

    init repo

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+// Back-end
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $data = Blok\Utils\Arr::csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ8Ggo8PUBJTujhMjsD0FjL2XIB6gtMfAsgGT5aiAGLqbOxIIUMwrzJHQOA2Rj4YTsujVAkpcuCw8qE/pub?gid=0&single=true&output=csv', [
@@ -11,8 +13,12 @@ $data = Blok\Utils\Arr::csvToArray('https://docs.google.com/spreadsheets/d/e/2PA
     'indexFromFirstRow' => true
 ]);
 
+/**
+ * @see : https://github.com/kint-php/kint
+ */
 //Kint::dump($data);
 ?>
+<!-- FRONT-END -->
 <!doctype html>
 <html lang="en">
 <head>
@@ -22,6 +28,7 @@ $data = Blok\Utils\Arr::csvToArray('https://docs.google.com/spreadsheets/d/e/2PA
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Test</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css">
 </head>
 <body>
 <table class="table table-stripped">
@@ -31,6 +38,35 @@ $data = Blok\Utils\Arr::csvToArray('https://docs.google.com/spreadsheets/d/e/2PA
  }
 ?>
 </table>
+
+<br><br>
+
+<canvas id="myChart"></canvas>
+
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js"></script>
+<script>
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45]
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+</script>
 </body>
 </html>
